723d8305d2b5af8cfc8539a1004c5163249c5ef2
Tim Jones <tgjonesuk@gmail.com>
2019-01-25 12:03:42 +0000

    Add Model Air and export function

--- data/transform.js
+++
@@ -1,6 +0,0 @@
-// Split tags/colours into array
-db.paints.find().forEach(el => {
-    el.tags = el.tags.split(",").map(s => s.trim());
-    el.colours = el.colours.split(",").map(s => s.trim());
-    db.paints.save(el);
-});
