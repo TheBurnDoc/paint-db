efdd5f4f8e901c286f7a04361ee12790d58a5594
Tim Jones <tgjonesuk@gmail.com>
2019-01-25 12:45:12 +0000

    Use JSON export format, not CSV

--- data/transform.js
+++
@@ -1,8 +0,0 @@
-// Split tags/colours into array
-db.paints.find().forEach(el => {
-    el.tags = el.tags.replace(/[^a-zA-Z,]/g, '');
-    el.colours = el.colours.replace(/[^a-zA-Z,]/g, '');
-    el.tags = el.tags.split(",").map(s => s.trim());
-    el.colours = el.colours.split(",").map(s => s.trim());
-    db.paints.save(el);
-}); 
